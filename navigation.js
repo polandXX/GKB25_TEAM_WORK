cd3fbb40169cbc9d47ce501fc70596f01f4230c1
MrBarS1 <46891200+MrBarS1@users.noreply.github.com>
2023-04-06 21:55:06 +0700

    Add files via upload

---
+++ navigation.js
@@ -0,0 +1,509 @@
+//назад
+var url_main ='Main.html';
+
+//главная
+var url_site = 'site.html';
+var url_navigation = 'navigation.html';
+var url_for_patient = 'for_pacient.html';
+var url_norm_documen = 'doki.html';
+var url_contact = 'cont.html';
+var url_mkc = 'MKC.html';
+var url_corruption = 'Corrupt.html';
+
+
+//МКЦ
+var phons_ruk_sostava = '14_mkc_phons_ruk_sostava.html';
+var phons_ruk_sostava = '14_mkc_phons_ruk_sostava.html';
+var phons_ruk_sostava = '14_mkc_phons_ruk_sostava.html';
+
+var poradok_and_uslovia = '13_mkc_poradok_and_uslovia.html';
+
+var uslovia = '12_mkc_uslovia.html';
+var uslovia2 = '12_mkc_uslovia_second.html';
+
+var inner_rules = '11_mkc_inner_rules.html';
+var inner_rules2 = '11_mkc_inner_rules_second.html';
+var inner_rules3 = '11_mkc_inner_rules_third.html';
+
+var Preiskurant = '10_Preiskurant.html';
+var Preiskurant2 = '10_Preiskurant2.html';
+var Preiskurant3 = '10_Preiskurant3.html';
+var Preiskurant4 = '10_Preiskurant4.html';
+var Preiskurant5 = '10_Preiskurant5.html';
+var Preiskurant6 = '10_Preiskurant6.html';
+
+var pol_plat_med = '9_pol_plat_med.html';
+var pol_plat_med2 = '9_pol_plat_med_second.html';
+var pol_plat_med3 = '9_pol_plat_med_third.html';
+
+var EGRUL = '8_EGRUL.html';
+var phones_up_org = '7_phones_up_org.html';
+
+var konsul = '3_konsul.html';
+
+var grafic = '2_grafic.html';
+
+var med_lic1 = '4_first_page.html'
+var med_lic2 = '4_second_page.html'
+var med_lic3 = '4_third_page.html'
+var med_lic4 = '4_four_page.html'
+var med_lic5 = '4_fifth_page.html'
+var med_lic6 = '4_six_page.html'
+var med_lic7 = '4_seven_page.html'
+var med_lic8 = '4_eight_page.html'
+var med_lic9 = '4_nine_page.html'
+var med_lic10 = '4_ten_page.html'
+
+//лицензии
+var url_lic1 = 'licenses_first_page.html';
+var url_lic2 = 'licenses_second_page.html';
+var url_lic3 = 'licenses_third_page.html';
+var url_lic4 = 'licenses_four_page.html';
+var url_lic5 = 'licenses_fifth_page.html';
+var url_lic6 = 'licenses_six_page.html';
+var url_lic7 = 'licenses_seven_page.html';
+var url_lic8 = 'licenses_eight_page.html';
+var url_lic9 = 'licenses_nine_page.html';
+var url_lic10 = 'licenses_ten_page.html';
+
+//все страницы OMC2
+var url_svid = 'svidetel.html'
+var url_help = 'help.html'
+var url_adresa ='adresa.html'
+var url_rabota ='rabota.html'
+var url_pass = 'pass.html'
+var url_usl = 'usl.html'
+var url_prava = 'prava.html'
+var url_nomera = 'nomera.html'
+var url_categ = 'category.html'
+var url_pere2 = 'pere2.html'
+var url_pere2_2 = 'pere2_2.html'
+
+var url_pere3 = 'pere3.html'
+var url_pere3_2 = 'pere3_2.html'
+var url_pere3_3 = 'pere3_3.html'
+var url_pere3_4 = 'pere3_4.html'
+var url_pere3_5 = 'pere3_5.html'
+var url_pere3_6 = 'pere3_6.html'
+var url_pere3_7 = 'pere3_7.html'
+var url_pere3_8 = 'pere3_8.html'
+var url_pere3_9 = 'pere3_9.html'
+var url_pere3_10 = 'pere3_10.html'
+var url_pere3_11 = 'pere3_11.html'
+var url_pere3_12 = 'pere3_12.html'
+var url_pere3_13 = 'pere3_13.html'
+var url_pere3_14 = 'pere3_14.html'
+
+var url_vid = 'vid.html'
+
+//все страницы OMC3
+var url_por = '301_omc3_por.html'
+var url_ruk = '302_omc3_rul.html'
+var url_por2 = '303_omc3_por2.html'
+var url_prav2 = '304_omc3_prev2.html'
+var url_por3 = '305_omc3_por3.html'
+var ulr_pol = '306_omc3_pol.html'
+var url_pam = '307_omc3_pam.html'
+var url_prav3 = '308_omc3_prav3.html'
+var url_por4 = '309_omc3_por4.html'
+var url_reg = '310_omc3_reg.html'
+var url_sroki = '311_omc3_sroki.html'
+var url_prav4 = '312_omc3_prav4.html'
+var url_por5 = '313_omc3_por5.html'
+
+//все страницы OMC4
+var url_med = '401_omc4_medrab.html'
+var url_zak1 = '402_omc4_zak1.html'
+var url_zak2 = '403_omc4_zak2.html'
+var url_zak3 = '404_omc4_zak3.html'
+var url_infa = '405_omc4_infa.html'
+var url_por6 = '406_omc4_por6.html'
+var url_por7 = '407_omc4_por7.html'
+var url_infa2 = '408_omc4_infa2.html'
+
+//территориальная программа
+var url_ter1 = 'territorial_first_page.html'
+var url_ter2 = 'territorial_second_page.html'
+var url_ter3 = 'territorial_third_page.html'
+var url_ter4 = 'territorial_four_page.html'
+var url_ter5 = 'territorial_fifth_page.html'
+var url_ter6 = 'territorial_six_page.html'
+var url_ter7 = 'territorial_seven_page.html'
+var url_ter8 = 'territorial_eight_page.html'
+var url_ter9 = 'territorial_nine_page.html'
+var url_ter10 = 'territorial_ten_page.html'
+var url_ter11 = 'territorial_eleven_page.html'
+var url_ter12 = 'territorial_tvelve_page.html'
+
+
+//по всем OMC
+var url_OMC_1P ='OMC_first_page.html';
+var url_OMC_2P ='OMC_second_page.html';
+var url_OMC_3P ='OMC_third_page.html';
+var url_OMC_4P ='OMC_four_page.html';
+
+//Corrupt
+var url_cor1 = '801_cor.html';
+var url_cor2 = '802_cor.html';
+var url_cor3 = '803_cor.html';
+var url_cor4 = '804_cor.html';
+var url_cor5 = '805_cor.html';
+var url_cor6 = '806_cor.html';
+
+//for_pacient
+var url_for_pacient1 = 'for_patient1'
+var url_for_pacient2 = 'for_patient2'
+var url_for_pacient3 = 'for_patient3'
+var url_for_pacient4 = 'for_patient4'
+var url_for_pacient5 = 'for_patient5'
+var url_for_pacient6 = 'for_patient6'
+var url_for_pacient7 = 'for_patient7'
+var url_for_pacient8 = 'for_patient8'
+var url_for_pacient9 = 'for_patient9'
+var url_for_pacient10 = 'for_patient10'
+var url_for_pacient11 = 'for_patient11'
+var url_for_pacient12 = 'for_patient12'
+var url_for_pacient13 = 'for_patient13'
+var url_for_pacient14 = 'for_patient14'
+var url_for_pacient15 = 'for_patient15'
+
+function OPEN_INDEX(id){
+    if(id === 'site'){
+        window.location.href = url_site;
+    }
+    else if(id === 'navigation'){
+        window.location.href = url_navigation;
+    }
+    else if(id === 'for_patient'){
+        window.location.href = url_for_patient;
+    }
+    else if(id === 'norm_document'){
+        window.location.href = url_norm_documen;
+    }
+    else if(id === 'omc'){
+        window.location.href = url_OMC_1P;
+    }
+    else if(id === 'contact'){
+        window.location.href = url_contact;
+    }
+    else if(id === 'mkc'){
+        window.location.href = url_mkc;
+    }
+    else if(id === 'corruption'){
+        window.location.href = url_corruption;
+    }
+    else if(id === 'main'){
+        window.location.href = url_main;
+    }
+    else if(id === 'OMC_2P'){
+        window.location.href = url_OMC_2P;
+    }
+    else if(id === 'OMC_3P'){
+        window.location.href = url_OMC_3P;
+    }
+    else if(id === 'OMC_4'){
+        window.location.href = url_OMC_4P;
+    }
+    else if(id === 'doki'){
+        window.location.href = url_lic1;
+    }
+    else if(id === 'lic1'){
+        window.location.href = url_lic2;
+    }
+    else if(id === 'lic2'){
+        window.location.href = url_lic3;
+    }
+    else if(id === 'lic3'){
+        window.location.href = url_lic4;
+    }
+    else if(id === 'lic4'){
+        window.location.href = url_lic5;
+    }
+    else if(id === 'lic5'){
+        window.location.href = url_lic6;
+    }
+    else if(id === 'lic6'){
+        window.location.href = url_lic7;
+    }
+    else if(id === 'lic7'){
+        window.location.href = url_lic8;
+    }
+    else if(id === 'lic8'){
+        window.location.href = url_lic9;
+    }
+    else if(id === 'lic9'){
+        window.location.href = url_lic10;
+    }
+    else if(id === 'lic10'){
+        window.location.href = url_lic9;
+    }
+    
+    else if(id === 'ter1'){
+        window.location.href = url_ter2;
+    }
+    else if(id === 'ter2'){
+        window.location.href = url_ter3;
+    }
+    else if(id === 'ter3'){
+        window.location.href = url_ter4;
+    }
+    else if(id === 'ter4'){
+        window.location.href = url_ter5;
+    }
+    else if(id === 'ter5'){
+        window.location.href = url_ter6;
+    }
+    else if(id === 'ter6'){
+        window.location.href = url_ter7;
+    }
+    else if(id === 'ter7'){
+        window.location.href = url_ter8;
+    }
+    else if(id === 'ter8'){
+        window.location.href = url_ter9;
+    }
+    else if(id === 'ter9'){
+        window.location.href = url_ter10;
+    }
+    else if(id === 'ter10'){
+        window.location.href = url_ter11;
+    }
+    else if(id === 'ter11'){
+        window.location.href = url_ter12;
+    }
+
+//OMC2
+    else if(id === 'b1'){
+        window.location.href = url_svid;
+    }
+    else if(id === 'b2'){
+        window.location.href = url_ter1;
+    }
+    else if(id === 'b3'){
+        window.location.href = url_help;
+    }
+    else if(id === 'b4'){
+        window.location.href = url_usl;
+    }
+    else if(id === 'b5'){
+        window.location.href = url_pere;
+    }
+    else if(id === 'b6'){
+        window.location.href = url_pere2;
+    }
+    else if(id === 'pere2'){
+        window.location.href = url_pere2_2;
+    }
+    else if(id === 'b7'){
+        window.location.href = url_pere3;
+    }
+    else if(id === 'b8'){
+        window.location.href = url_adresa;
+    }
+    else if(id === 'b9'){
+        window.location.href = url_rabota;
+    }
+    else if(id === 'b10'){
+        window.location.href = url_pass;
+    }
+    else if(id === 'b11'){
+        window.location.href = url_prava;
+    }
+    else if(id === 'b12'){
+        window.location.href = url_nomera;
+    }
+    else if(id === 'b13'){
+        window.location.href = url_vid;
+    }
+    else if(id === 'b14'){
+        window.location.href = url_categ;
+    }
+    
+//OMC3
+    else if(id === 'b15'){
+        window.location.href = url_svid;
+    }
+    else if(id === 'b16'){
+        window.location.href = url_ter1;
+    }
+    else if(id === 'b17'){
+        window.location.href = url_help;
+    }
+    else if(id === 'b18'){
+        window.location.href = url_usl;
+    }
+    else if(id === 'b19'){
+        window.location.href = url_pere;
+    }
+    else if(id === 'b20'){
+        window.location.href = url_pere2;
+    }
+    else if(id === 'b21'){
+        window.location.href = url_pere2_2;
+    }
+    else if(id === 'b22'){
+        window.location.href = url_pere3;
+    }
+    else if(id === 'b23'){
+        window.location.href = url_adresa;
+    }
+    else if(id === 'b24'){
+        window.location.href = url_rabota;
+    }
+    else if(id === 'b25'){
+        window.location.href = url_pass;
+    }
+    else if(id === 'b26'){
+        window.location.href = url_prava;
+    }
+    else if(id === 'b27'){
+        window.location.href = url_nomera;
+    }
+
+
+    else if(id === 'omc1'){
+        window.location.href = url_OMC_2P;
+    }
+    else if(id === 'omc2'){
+        window.location.href = url_OMC_3P;
+    }
+    else if(id === 'omc3'){
+        window.location.href = url_OMC_4P;
+    }
+
+
+    else if(id === 'pere3'){
+        window.location.href = url_pere3_2;
+    }
+    else if(id === 'pere3_2'){
+        window.location.href = url_pere3_3;
+    }
+    else if(id === 'pere3_3'){
+        window.location.href = url_pere3_4;
+    }
+    else if(id === 'pere3_4'){
+        window.location.href = url_pere3_5;
+    }
+    else if(id === 'pere3_5'){
+        window.location.href = url_pere3_6;
+    }
+    else if(id === 'pere3_6'){
+        window.location.href = url_pere3_7;
+    }
+    else if(id === 'pere3_7'){
+        window.location.href = url_pere3_8;
+    }
+    else if(id === 'pere3_8'){
+        window.location.href = url_pere3_9;
+    }
+    else if(id === 'pere3_9'){
+        window.location.href = url_pere3_10;
+    }
+    else if(id === 'pere3_10'){
+        window.location.href = url_pere3_11;
+    }
+    else if(id === 'pere3_11'){
+        window.location.href = url_pere3_12;
+    }
+    else if(id === 'pere3_12'){
+        window.location.href = url_pere3_13;
+    }
+    else if(id === 'pere3_13'){
+        window.location.href = url_pere3_14;
+    }
+
+
+    else if(id === '14_mkc_phons_ruk_sostava'){
+        window.location.href = phons_ruk_sostava;
+    }
+    else if(id === '11_mkc_inner_rules'){
+        window.location.href = inner_rules;
+    }
+    else if(id === '11_mkc_inner_rules_second'){
+        window.location.href = inner_rules2;
+    }
+    else if(id === '11_mkc_inner_rules_third'){
+        window.location.href = inner_rules3;
+    }
+    else if(id === '8_EGRUL'){
+        window.location.href = EGRUL;
+    }
+    else if(id === '7_phones_up_org'){
+        window.location.href = phones_up_org;
+    }
+    else if(id === '9_pol_plat_med'){
+        window.location.href = pol_plat_med;
+    }
+    else if(id === '9_pol_plat_med_second'){
+        window.location.href = pol_plat_med2;
+    }
+    else if(id === '9_pol_plat_med_third'){
+        window.location.href = pol_plat_med3;
+    }
+    else if(id === '2_grafic'){
+        window.location.href = grafic;
+    }
+    else if(id === '3_konsul'){
+        window.location.href = konsul;
+    }
+    else if(id === '4_first_page'){
+        window.location.href = med_lic1;
+    }
+    else if(id === '4_second_page'){
+        window.location.href = med_lic2;
+    }
+    else if(id === '4_third_page'){
+        window.location.href = med_lic3;
+    }
+    else if(id === '4_four_page'){
+        window.location.href = med_lic4;
+    }
+    else if(id === '4_fifth_page'){
+        window.location.href = med_lic5;
+    }
+    else if(id === '4_six_page'){
+        window.location.href = med_lic6;
+    }
+    else if(id === '4_seven_page'){
+        window.location.href = med_lic7;
+    }
+    else if(id === '4_eight_page'){
+        window.location.href = med_lic8;
+    }
+    else if(id === '4_nine_page'){
+        window.location.href = med_lic9;
+    }
+    else if(id === '4_ten_page'){
+        window.location.href = med_lic10;
+    }
+
+    else if(id === '12_mkc_uslovia'){
+        window.location.href = uslovia;
+    }
+    else if(id === '12_mkc_uslovia_second'){
+        window.location.href = uslovia2;
+    }
+    else if(id === '13_mkc_poradok_and_uslovia'){
+        window.location.href = poradok_and_uslovia;
+    }
+    else if(id === '10_Preiskurant'){
+        window.location.href = Preiskurant;
+    }
+    else if(id === '10_Preiskurant2'){
+        window.location.href = Preiskurant2;
+    }
+    else if(id === '10_Preiskurant3'){
+        window.location.href = Preiskurant3;
+    }
+    else if(id === '10_Preiskurant4'){
+        window.location.href = Preiskurant4;
+    }
+    else if(id === '10_Preiskurant5'){
+        window.location.href = Preiskurant5;
+    }
+    else if(id === '10_Preiskurant6'){
+        window.location.href = Preiskurant6;
+    }
+}
+
+
